d506838e6a42a4a3e207509429e65746a9dfc172
Ltrez <93102797+Ltrez@users.noreply.github.com>
2026-03-18 20:49:07 -0400

    Update piece.py

--- src/piece.py
+++ src/piece.py
@@ -1,1 +1,25 @@
 
+from enum import Enum
+
+
+class Color(Enum):
+    WHITE = 1
+    BLACK = -1
+
+
+class PieceType(Enum):
+    PAWN = "P"
+    KNIGHT = "N"
+    BISHOP = "B"
+    ROOK = "R"
+    QUEEN = "Q"
+    KING = "K"
+
+
+class Piece:
+    def __init__(self, piece_type: PieceType, color: Color):
+        self.piece_type = piece_type
+        self.color = color
+
+    def __repr__(self):
+        return f"{self.color.name[0]}{self.piece_type.value}"
